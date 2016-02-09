f4d0f012c1516d7e72e2d0f622155bc996835c51
Cornelius Toole <cornelius.toole@gmail.com>
2016-02-09 09:23:51 -0600

    Task #1: create Television.java, add instance vars
    C:\Users\ctoole\Documents\Television.java:5: error: variable MANUFACTURER not initialized in the default constructor
      private final String MANUFACTURER;
                           ^
    C:\Users\ctoole\Documents\Television.java:7: error: variable SCREEN_SIZE not initialized in the default constructor
      private final int SCREEN_SIZE;

---
+++ Television.java
@@ -0,0 +1,18 @@
+// The purpose of this class is to model a television
+// Cornelius Toole 2/9/16
+public class Television{
+  // the television's brand name
+  private final String MANUFACTURER;
+  // the television's diagnal screen size
+  private final int SCREEN_SIZE;
+  // the power state of the television
+  private boolean powerOn;
+  // the station currently tuned to by the TV
+  private int channel;
+  // the TV's current loudness (0 means no sound)
+  private int volume;
+  
+  
+
+
+}
